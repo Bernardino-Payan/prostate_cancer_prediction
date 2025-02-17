e8507f025ea048e96d6df556891a2a9c8e6084c0
Bernardino Payan <bernardinopayan@Bernardinos-MacBook-Pro.local>
2025-02-17 17:16:28 -0500

    Resolved merge conflict and added index.html

--- prostate_cancer_prediction/pcancer.py
+++ prostate_cancer_prediction/pcancer.py
@@ -58,7 +58,11 @@ def predict_json():
     try:
         # Expecting JSON input
         data = request.get_json()
+<<<<<<< HEAD
 
+=======
+        
+>>>>>>> 453e0e2 (Added missing index.html to templates directory)
         # Extracting only the necessary top 10 features
         features = [float(data.get(feature, 0)) for feature in TOP_10_FEATURES]  # Default to 0 if missing
 
@@ -76,3 +80,7 @@ def predict_json():
 # Run the app
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
+<<<<<<< HEAD
+=======
+
+>>>>>>> 453e0e2 (Added missing index.html to templates directory)
